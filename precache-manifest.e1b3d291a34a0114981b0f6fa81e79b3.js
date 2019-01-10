d1917e83778ec0a84cbb3009161773f133663031
Andrew Woo <andrew.woo@verizon.net>
2019-01-09 22:02:38 -0500

    Updates

---
+++ precache-manifest.e1b3d291a34a0114981b0f6fa81e79b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc4672f8c980817fa3df",
+    "url": "/standup-helper/static/css/main.a6e618e7.chunk.css"
+  },
+  {
+    "revision": "dc4672f8c980817fa3df",
+    "url": "/standup-helper/static/js/main.dc4672f8.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/standup-helper/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "78ec63f17b83b863f586",
+    "url": "/standup-helper/static/js/runtime~main.78ec63f1.js"
+  },
+  {
+    "revision": "428fa1edd7ca3003ae8c87d603ad19b0",
+    "url": "/standup-helper/index.html"
+  }
+];
