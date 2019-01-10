55d34fff5471005dbdff0b51aba16c14cc3b9f7a
Andrew Woo <andrew.woo@verizon.net>
2019-01-09 21:55:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [
+      "Andrew",
+      "Damiana",
+      "Erin",
+      "Raynaldo",
+      "Franklin",
+      "Kimberly",
+      "Sudheer",
+      "Joseph",
+      "Karpagaraj",
+      "Purushothaman",
+      "Karthik"
+    ]
+  };
+
+  removeUser = e => {
+    const u = [...this.state.users];
+    let index = u.indexOf(e.target.value);
+    u.splice(index, 1);
+    this.setState({ users: u });
+  };
+
   render() {
+    let users = this.state.users.map(user => {
+      return (
+        <button
+          className="App-button"
+          key={user}
+          onClick={this.removeUser}
+          value={user}
+        >
+          {user}
+        </button>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>MUGA Standup Helper</h1>
         </header>
+        {users}
       </div>
     );
   }
